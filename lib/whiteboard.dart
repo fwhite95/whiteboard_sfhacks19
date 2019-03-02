7b593cd18abe77dcaf96677a31ebd0e8b44d8beb
Freddie White <white.freddie95@gmail.com>
2019-03-02 15:41:42 -0800

    hgh

--- lib/whiteboard.dart
+++ lib/whiteboard.dart
@@ -26,8 +26,20 @@ class _WhiteBoardPageState extends State<WhiteBoardPage>{
   PainterController _controller;
   bool _finished;
 
-  void _onUndoButtonPressed(){
+  Widget _onListButtonPressed(){
+    return new Column(
+      children: <Widget>[
+        FloatingActionButton(
+          onPressed: _controller.undo,
+          child: const Icon(Icons.undo, size: 36.0,),
+        ),
+        FloatingActionButton(
+          onPressed: _controller.clear,
+          child: const Icon(Icons.clear, size: 36.0,),
+        ),
 
+      ],
+    );
   }
 
   @override
@@ -64,14 +76,9 @@ class _WhiteBoardPageState extends State<WhiteBoardPage>{
               child: Column(
                 children: <Widget>[
                   FloatingActionButton(
-                    onPressed: _controller.undo,
-                    child: const Icon(Icons.undo, size: 36.0,),
+                    onPressed: _onListButtonPressed,
+                    child: const Icon(Icons.add, size: 36.0,),
                   ),
-                  FloatingActionButton(
-                    onPressed: _controller.clear,
-                    child: const Icon(Icons.clear, size: 36.0,),
-                  ),
-
                 ],
               ),
             ),
